c6d66d76ac39988b0f7751ee5c299f5df15d538c
rajkumarmarichamy <twdraji1999@gmail.com>
2022-11-17 12:49:54 +0530

    2way accessing

---
+++ java script/javaScript Basics/2way_accessing_of_element.js
@@ -0,0 +1,14 @@
+const person = {
+  firstName: "raj",
+  lastName: "kumar",
+};
+
+//first way
+
+console.log(person.firstName);
+console.log(person.lastName);
+
+//second way
+
+console.log(person["firstName"]);
+console.log(person["lastName"]);
